7b0d8da10f20b644cfff80d86bdb832f125bc02f
Rúben Sousa <ruben@rubensousa.com>
2022-08-24 23:15:15 +0200

    Clean docs of OnViewHolderSelectedListener

--- dpadrecyclerview/src/main/java/com/rubensousa/dpadrecyclerview/OnViewHolderSelectedListener.kt
+++ dpadrecyclerview/src/main/java/com/rubensousa/dpadrecyclerview/OnViewHolderSelectedListener.kt
@@ -4,30 +4,24 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 /**
- * Interface for receiving notification when a child of this ViewGroup has been selected.
+ * Interface for receiving a notification when a ViewHolder has been selected.
  * There are two methods:
  *
- *  * [onViewHolderSelected] is called when the view holder is about to be selected.
- *  The listener could change size of the view holder in this callback.
+ *  * [onViewHolderSelected] is called when the ViewHolder has been selected
  *
- *  * [onViewHolderSelectedAndAligned] is called when view holder has been selected
- *  and laid out in RecyclerView.
+ *  * [onViewHolderSelectedAndAligned] is called when the ViewHolder has been selected
+ *  and aligned out to its final position
  */
-// TODO Clear docs
 interface OnViewHolderSelectedListener {
 
     /**
-     * Callback method to be invoked when a child of this ViewGroup has been selected. Listener
-     * might change the size of the child and the position of the child is not finalized. To get
-     * the final layout position of child, override [.onChildViewHolderSelectedAndPositioned].
-     *
      * @param parent      The RecyclerView where the selection happened.
      * @param child       The ViewHolder within the RecyclerView that is selected, or null if no
      * view is selected.
      * @param position    The position of the view in the adapter, or NO_POSITION
      * if no view is selected.
-     * @param subPosition The index of which [ItemAlignmentDef] being used,
-     * 0 if there is no ItemAlignmentDef defined for the item.
+     * @param subPosition The index of the alignment from [DpadViewHolder.getAlignments],
+     * or 0 if there is no custom alignment
      */
     fun onViewHolderSelected(
         parent: RecyclerView,
@@ -38,16 +32,15 @@ interface OnViewHolderSelectedListener {
     }
 
     /**
-     * Callback method to be invoked when a child of this ViewGroup has been selected and
-     * aligned to its final position.
+     * Called when [child] has scrolled to its final position
      *
      * @param parent      The RecyclerView where the selection happened.
      * @param child       The ViewHolder within the RecyclerView that is selected, or null if no
      * view is selected.
      * @param position    The position of the view in the adapter, or NO_POSITION
      * if no view is selected.
-     * @param subPosition The index of which [ItemAlignmentDef] being used,
-     * 0 if there is no ItemAlignmentDef defined for the item.
+     * @param subPosition The index of the alignment from [DpadViewHolder.getAlignments],
+     * or 0 if there is no custom alignment
      */
     fun onViewHolderSelectedAndAligned(
         parent: RecyclerView,
@@ -56,4 +49,5 @@ interface OnViewHolderSelectedListener {
         subPosition: Int
     ) {
     }
+
 }
