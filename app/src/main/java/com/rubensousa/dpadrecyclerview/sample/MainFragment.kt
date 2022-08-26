2edf8ba94e74aa7c141da4850abe4f6080fdbac2
Rúben Sousa <ruben@rubensousa.com>
2022-08-27 01:01:07 +0200

    Add placeholder until content is loading

--- app/src/main/java/com/rubensousa/dpadrecyclerview/sample/MainFragment.kt
+++ app/src/main/java/com/rubensousa/dpadrecyclerview/sample/MainFragment.kt
@@ -3,15 +3,17 @@ package com.rubensousa.dpadrecyclerview.sample
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.rubensousa.decorator.LinearMarginDecoration
 import com.rubensousa.dpadrecyclerview.*
 import com.rubensousa.dpadrecyclerview.databinding.ScreenTvNestedListsBinding
-import com.rubensousa.dpadrecyclerview.sample.list.DpadStateHolder
 import com.rubensousa.dpadrecyclerview.sample.item.ItemViewHolder
-import com.rubensousa.dpadrecyclerview.sample.list.ListModel
+import com.rubensousa.dpadrecyclerview.sample.list.DpadStateHolder
 import com.rubensousa.dpadrecyclerview.sample.list.NestedListAdapter
+import com.rubensousa.dpadrecyclerview.sample.list.ListPlaceholderAdapter
 import timber.log.Timber
 
 class MainFragment : Fragment(R.layout.screen_tv_nested_lists) {
@@ -20,12 +22,26 @@ class MainFragment : Fragment(R.layout.screen_tv_nested_lists) {
     private val binding: ScreenTvNestedListsBinding get() = _binding!!
     private var selectedPosition = RecyclerView.NO_POSITION
     private val scrollStateHolder = DpadStateHolder()
+    private val viewModel by viewModels<MainViewModel>()
+    private val concatAdapter = ConcatAdapter(
+        ConcatAdapter.Config.Builder()
+            .setIsolateViewTypes(true)
+            .build()
+    )
+    private val loadingAdapter = ListPlaceholderAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = ScreenTvNestedListsBinding.bind(view)
-        setupAdapter()
+        val nestedListAdapter = setupAdapter()
         setupAlignment(binding.recyclerView)
+        setupPagination(binding.recyclerView)
+        viewModel.listState.observe(viewLifecycleOwner) { list ->
+            nestedListAdapter.submitList(list)
+        }
+        viewModel.loadingState.observe(viewLifecycleOwner) { isLoading ->
+            loadingAdapter.show(isLoading)
+        }
         binding.recyclerView.requestFocus()
         if (selectedPosition != RecyclerView.NO_POSITION) {
             binding.recyclerView.setSelectedPosition(
@@ -39,27 +55,50 @@ class MainFragment : Fragment(R.layout.screen_tv_nested_lists) {
         }
     }
 
-    private fun setupAdapter() {
+    private fun setupAdapter(): NestedListAdapter {
         val nestedListAdapter = NestedListAdapter(scrollStateHolder,
             object : ItemViewHolder.ItemClickListener {
                 override fun onViewHolderClicked() {
                     findNavController().navigate(R.id.open_detail)
                 }
             })
-        val list = ArrayList<ListModel>()
-        for (i in 0 until 100) {
-            list.add(generateList("List $i"))
-        }
-        nestedListAdapter.submitList(list)
+        concatAdapter.addAdapter(nestedListAdapter)
+        concatAdapter.addAdapter(loadingAdapter)
         nestedListAdapter.stateRestorationPolicy =
             RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
-        binding.recyclerView.adapter = nestedListAdapter
+        binding.recyclerView.adapter = concatAdapter
+        return nestedListAdapter
+    }
+
+    private fun setupPagination(recyclerView: DpadRecyclerView) {
+        recyclerView.addOnViewHolderSelectedListener(object :
+            OnViewHolderSelectedListener {
+            override fun onViewHolderSelected(
+                parent: RecyclerView,
+                child: RecyclerView.ViewHolder?,
+                position: Int,
+                subPosition: Int
+            ) {
+                selectedPosition = position
+                viewModel.loadMore(selectedPosition)
+                Timber.d("Selected: $position, $subPosition")
+            }
+
+            override fun onViewHolderSelectedAndAligned(
+                parent: RecyclerView,
+                child: RecyclerView.ViewHolder?,
+                position: Int,
+                subPosition: Int
+            ) {
+                Timber.d("Aligned: $position, $subPosition")
+            }
+        })
     }
 
     private fun setupAlignment(recyclerView: DpadRecyclerView) {
         recyclerView.setParentAlignment(
             ParentAlignment(
-                edge = ParentAlignment.Edge.MIN_MAX,
+                edge = ParentAlignment.Edge.MIN,
                 offset = 0,
                 offsetRatio = 0.5f
             )
@@ -75,39 +114,11 @@ class MainFragment : Fragment(R.layout.screen_tv_nested_lists) {
                 )
             )
         )
-        recyclerView.addOnViewHolderSelectedListener(object :
-            OnViewHolderSelectedListener {
-            override fun onViewHolderSelected(
-                parent: RecyclerView,
-                child: RecyclerView.ViewHolder?,
-                position: Int,
-                subPosition: Int
-            ) {
-                selectedPosition = position
-                Timber.d("Selected: $position, $subPosition")
-            }
-
-            override fun onViewHolderSelectedAndAligned(
-                parent: RecyclerView,
-                child: RecyclerView.ViewHolder?,
-                position: Int,
-                subPosition: Int
-            ) {
-                Timber.d("Aligned: $position, $subPosition")
-            }
-        })
-    }
-
-    private fun generateList(title: String): ListModel {
-        val items = ArrayList<Int>()
-        repeat(100) {
-            items.add(it)
-        }
-        return ListModel(title, items)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.recyclerView.adapter = null
         _binding = null
     }
 
---
+++ app/src/main/java/com/rubensousa/dpadrecyclerview/sample/MainViewModel.kt
@@ -0,0 +1,56 @@
+package com.rubensousa.dpadrecyclerview.sample
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rubensousa.dpadrecyclerview.sample.list.ListModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val list = ArrayList<ListModel>()
+    private val listLiveData = MutableLiveData<List<ListModel>>()
+    private val loadingStateLiveData = MutableLiveData<Boolean>()
+    val loadingState: LiveData<Boolean> = loadingStateLiveData
+    val listState: LiveData<List<ListModel>> = listLiveData
+
+    init {
+        for (i in 0 until 3) {
+            list.add(generateList("List $i"))
+        }
+        listLiveData.postValue(ArrayList(list))
+    }
+
+
+    fun loadMore(selectedPosition: Int) {
+        if (loadingState.value == true) {
+            return
+        }
+        val diffToEnd = list.size - selectedPosition
+        if (diffToEnd >= 2) {
+            return
+        }
+
+        loadingStateLiveData.postValue(true)
+        viewModelScope.launch(Dispatchers.Default) {
+            for (i in 0 until 2) {
+                list.add(generateList("List ${list.size}"))
+            }
+           // delay(2000L)
+            listLiveData.postValue(ArrayList(list))
+        }.invokeOnCompletion { loadingStateLiveData.postValue(false) }
+
+    }
+
+    private fun generateList(title: String): ListModel {
+        val items = ArrayList<Int>()
+        repeat(100) {
+            items.add(it)
+        }
+        return ListModel(title, items)
+    }
+
+
+}
