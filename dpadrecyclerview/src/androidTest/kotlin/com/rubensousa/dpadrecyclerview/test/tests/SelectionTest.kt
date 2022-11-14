a66d02ec7a85659f32861b5d6b837a410d443580
Rúben Sousa <ruben@rubensousa.com>
2022-11-14 02:44:59 +0100

    Do not use PivotLayoutManager in SelectionTest for now

--- dpadrecyclerview/src/androidTest/kotlin/com/rubensousa/dpadrecyclerview/test/tests/SelectionTest.kt
+++ dpadrecyclerview/src/androidTest/kotlin/com/rubensousa/dpadrecyclerview/test/tests/SelectionTest.kt
@@ -55,7 +55,7 @@ class SelectionTest : GridTest() {
 
     @Before
     fun setup() {
-        DpadRecyclerViewHelper.enableNewPivotLayoutManager(true)
+        DpadRecyclerViewHelper.enableNewPivotLayoutManager(false)
     }
 
     @Test
